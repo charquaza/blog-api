4fb2d27d9b483c4934ce460c71735d08049df015
Jonathan Park <johanpk10@gmail.com>
2021-09-23 18:57:20 -0700

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,48 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//authentication routes
+router.post('sign-up', function (req, res, next) {
+});
+router.post('log-in', function (req, res, next) {
+});
+router.post('log-out', function (req, res, next) {
+});
+
+//Posts routes
+router.get('/posts', function (req, res, next) {
+  //read all posts
+});
+router.post('/posts', function (req, res, next) {
+  //create new post
+});
+
+router.get('/posts/:id', function (req, res, next) {
+  //read one post
+});
+router.put('/posts/:id', function (req, res, next) {
+  //update one post
+});
+router.delete('/posts/:id', function (req, res, next) {
+  //delete one post
+});
+
+//Comments routes
+router.get('/comments', function (req, res, next) {
+  //read all comments
+});
+router.post('/comments', function (req, res, next) {
+  //create new comment
+});
+
+router.get('/comments/:id', function (req, res, next) {
+  //read one comment
+});
+router.put('/comments/:id', function (req, res, next) {
+  //update one comment
+});
+router.delete('/comments/:id', function (req, res, next) {
+  //delete one comment
+});
+
 module.exports = router;
